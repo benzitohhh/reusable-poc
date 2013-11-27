eb3b8e72c1b8a137c73ff3d216a6c08aadb5e580
Ben Immanuel <ben.immanuel@aistemos.com>
2013-11-27 14:40:06 +0000

    data done: generates series for by company, cluster, outcome

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,79 +1,192 @@
+fakeMissingGrantDates();
+var series_outcomes = getSeries_outcomes();
+var series_cluster = getSeries_cluster();
+var series_company = getSeries_company();
 
-// per outcome (bose, cluster 1 - fake some missing data)
-
-// per cluster (bose)
-
-// per company (bose, hiwave, and fake two other companies)
-
-
-// outcome
-var outcomes = get_outcome_series();
+// ================== CHART ==============================
 
 
 
-function get_outcome_series(){
+
+// ================== DATA ===============================
+
+/**
+ Returns outcome -> (year -> freq)
+ i.e. "outcome" is accepted/pending/expired
+*/
+function getSeries_outcomes(){
   var cluster = eqip.boseClusters[0].members;
 
-  // ***** TEMPORARY CODE TO SIMULATE VARIATION IN DATA. TODO: remove! *****
+  // get pFams
+  var pFams = cluster.map(function(pId) {
+                var p = eqip.bosePatFams[pId];
+                if (!p) { console.log("MISSING patFam: id=" + pId); }
+                return p;
+              }).filter(function(p) { return p != null; });
+
+  // Construct histograms: outcome -> (year -> freq)
+  var counts = {"grant":{}, "priority":{}, "priority_accepted":{}, "priority_pending":{}, "priority_expired":{} },
+      yearsSet = {},
+      currDate = new Date(),
+      secs_in_year = 1000 * 60 * 60 * 24 * 365,
+      inc = function(o, k) { o[k] = o[k] ? o[k] + 1 : 1; };
+  pFams.forEach(function(p) {
+          // for each patFam p in cluster...
+          // extract priority year (should always exist)
+          var priYr = p.priorityDate.slice(0, 4);
+          yearsSet[priYr] = true;
+          inc(counts['priority'], priYr);
+          if (p.grantDate) {
+            // Grant year exists (i.e. application was accepted)
+            var graYr = p.grantDate.slice(0, 4);
+            yearsSet[graYr] = true;
+            inc(counts['grant'], graYr);
+            inc(counts['priority_accepted'], priYr);
+          } else if (new Date(p.priorityDate).getTime() + 4 * secs_in_year < currDate) {
+            // Patent application has expired
+            // ie We assume applications automatically expire after 4 years
+            inc(counts['priority_expired'], priYr);
+          } else {
+            // Application may still be pending.
+            // It could also be rejected, we don't know from the data.
+            inc(counts['priority_pending'], priYr);
+          }
+        });
+
+  // Calculate x domain (range of years)
+  var yearsArr = d3.keys(yearsSet).sort();
+  var years = d3.range(+yearsArr[0], +yearsArr[yearsArr.length - 1] + 1);
+
+  // Transform histograms to series
+  var series = ["priority_accepted", "priority_pending", "priority_expired"].map(function(s) {
+    return {
+      name: s,
+      series: years.map(function(year) { return {x: year, y: counts[s][year] ? counts[s][year] : 0 }; })
+    };
+  });
+  
+  return series;
+}
+
+/**
+ Returns cluster -> (year -> freq)
+*/
+function getSeries_cluster() {
+  // get pFams per cluster
+  var clusters = eqip.boseClusters.reduce(function(acc, c, i) {
+    var pFams = c.members.map(function(pId) {
+      var p = eqip.bosePatFams[pId];
+      if (!p) { console.log("MISSING patFam: id=" + pId); }
+      return p; 
+    });
+    pFams = pFams.filter(function(p) { return p != null; }); // remove nulls
+    var name = "Cluster #" + i;
+    acc[name] = pFams;
+    return acc;
+  }, {});
+
+  // Construct histograms: cluster -> (year -> freq)
+  var counts = d3.keys(clusters).reduce(function(acc, c) { acc[c] = {}; return acc; }, {}),
+      yearsSet = {},
+      inc = function(o, k) { o[k] = o[k] ? o[k] + 1 : 1; };
+  d3.keys(clusters).forEach(function(c) {
+    clusters[c].forEach(function(p) {
+      var priYr = p.priorityDate.slice(0, 4);
+      yearsSet[priYr] = true;
+      inc(counts[c], priYr);
+    });
+  });
+
+  // Calculate x domain (range of years)
+  var yearsArr = d3.keys(yearsSet).sort();
+  var years = d3.range(+yearsArr[0], +yearsArr[yearsArr.length - 1] + 1);
+
+  // Transform histograms to series
+  var series = d3.keys(counts).map(function(cluster) {
+    return {
+      name:   cluster,
+      series: years.map(function(year) { return { x: year, y: counts[cluster][year] ? counts[cluster][year] : 0 }; })
+    }; 
+  });
+
+  return series;
+}
+
+/**
+ Returns company -> (year -> freq)
+ i.e. companies are bose, hiwave etc...
+*/
+function getSeries_company() {
+
+  function randomPriDate(priDateStr) {
+    // apply noise to priDate (ensure result within [1965, 2012]
+    var priYr = +priDateStr.slice(0, 4);
+    var delta_max = 2013 - priYr;
+    var delta_min = Math.min(0, 1965 - priYr);
+    var delta = Math.floor(delta_min + (delta_max - delta_min) * Math.random());
+    var nYear = priYr + delta;
+    return nYear + "" + priDateStr.slice(4);    
+  }
+  
+  function getFakeData(patFams) {
+    return patFams.map(function(p) {
+      var np = $.extend(true, {}, p);
+      np.priorityDate = randomPriDate(np.priorityDate);
+      return np;
+    });
+  }
+  
+  // Get pFams per company
+  var bose_pFams   = d3.values(eqip.bosePatFams);
+  var hiwave_pFams = d3.values(eqip.hiwavePatfams);
+  var companies = {
+    "Bose"            : bose_pFams,
+    "Hiwave"          : hiwave_pFams,
+    "global Omnicorp" : getFakeData(bose_pFams),
+    "Acme LLC"        : getFakeData(bose_pFams)
+  };
+
+  // Construct histograms: company -> (year -> freq)
+  var counts = d3.keys(companies).reduce(function(acc, d) { acc[d] = {}; return acc; }, {}),
+      yearsSet = {},
+      inc = function(o, k) { o[k] = o[k] ? o[k] + 1 : 1; };
+  d3.keys(companies).forEach(function(c) {
+    companies[c].forEach(function(p) {
+      var priYr = p.priorityDate.slice(0, 4);
+      yearsSet[priYr] = true;
+      inc(counts[c], priYr);
+    });
+  });
+
+  // Calculate x domain (range of years)
+  var yearsArr = d3.keys(yearsSet).sort();
+  var years = d3.range(+yearsArr[0], +yearsArr[yearsArr.length - 1] + 1);
+
+  // Transform histograms to series
+  var series = d3.keys(counts).map(function(company) {
+    return {
+      name: company,
+      series: years.map(function(year) { return { x: year, y: counts[company][year] ? counts[company][year] : 0}; })
+    };
+  });
+
+  return series;  
+}
+
+
+/*
+ Simulate variation in Bose Cluster 0,
+ by randomly removing some grant dates.
+*/
+function fakeMissingGrantDates(){
+  var cluster = eqip.boseClusters[0].members;
   // remove grant date for the first and last quarters
   cluster.slice(0, Math.ceil(cluster.length / 4))
     .concat(cluster.slice(cluster.length - Math.ceil(cluster.length / 4)))
     .forEach(function(pId) {
       delete eqip.bosePatFams[pId].grantDate;
     });
-  // ***** END OF TEMPORARY CODE *****
-
-  // Construct histograms
-  var hgrams = {"grant":{}, "priority":{}, "priority_accepted":{}, "priority_pending":{}, "priority_expired":{} },
-      yearsSet = {},
-      currDate = new Date(),
-      secs_in_year = 1000 * 60 * 60 * 24 * 365,
-      inc = function(o, k) { o[k] = o[k] ? o[k] + 1 : 1; };
-  cluster.map(function(pId) {
-           var p = eqip.bosePatFams[pId];
-           if (!p) { console.log("MISSING patFam: id=" + pId); }
-           return p;
-         })
-         .filter(function(p) { return p != null; })
-         .forEach(function(p) {
-           // for each patFam p in cluster...
-           // extract priority year (should always exist)
-           var priYr = p.priorityDate.slice(0, 4);
-           yearsSet[priYr] = true;
-           inc(hgrams['priority'], priYr);
-           if (p.grantDate) {
-             // Grant year exists (i.e. application was accepted)
-             var graYr = p.grantDate.slice(0, 4);
-             yearsSet[graYr] = true;
-             inc(hgrams['grant'], graYr);
-             inc(hgrams['priority_accepted'], priYr);
-           } else if (new Date(p.priorityDate).getTime() + 4 * secs_in_year < currDate) {
-             // Patent application has expired
-             // ie We assume applications automatically expire after 4 years
-             inc(hgrams['priority_expired'], priYr);
-           } else {
-             // Application may still be pending.
-             // It could also be rejected, we don't know from the data.
-             inc(hgrams['priority_pending'], priYr);
-           }
-         });
-
-  // Calculate x domain (discrete range of years)
-  var yearsArr = d3.keys(yearsSet).sort();
-  var years = d3.range(+yearsArr[0], +yearsArr[yearsArr.length - 1] + 1);
-
-  // TODO: remove unwanted series
-  // TODO: label them!
-
-  // Transform histograms to series
-  var series = d3.keys(hgrams).map(function(s) {
-    return years.map(function(year) {
-      return {x: year, y: hgrams[s][year] ? hgrams[s][year] : 0 };
-    });
-  });
-
-  return series;
-  
 }
+
 
 
