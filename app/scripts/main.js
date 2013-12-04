2cc67ce35ec89885234de101ccae31778cd22eca
Ben Immanuel <ben.immanuel@aistemos.com>
2013-12-04 11:20:29 +0000

    remoed unnused code

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,108 +1,3 @@
-// ================== DATA ===============================
-
-/**
- Returns cluster -> (year -> freq)
-*/
-function getSeries_cluster() {
-  // get pFams per cluster
-  var clusters = eqip.boseClusters.reduce(function(acc, c, i) {
-    var pFams = c.members.map(function(pId) {
-      var p = eqip.bosePatFams[pId];
-      if (!p) { console.log("MISSING patFam: id=" + pId); }
-      return p; 
-    });
-    pFams = pFams.filter(function(p) { return p != null; }); // remove nulls
-    var name = "Cluster #" + (i + 1);
-    acc[name] = pFams;
-    return acc;
-  }, {});
-
-  // Construct histograms: cluster -> (year -> freq)
-  var counts = d3.keys(clusters).reduce(function(acc, c) { acc[c] = {}; return acc; }, {}),
-      yearsSet = {},
-      inc = function(o, k) { o[k] = o[k] ? o[k] + 1 : 1; };
-  d3.keys(clusters).forEach(function(c) {
-    clusters[c].forEach(function(p) {
-      var priYr = p.priorityDate.slice(0, 4);
-      yearsSet[priYr] = true;
-      inc(counts[c], priYr);
-    });
-  });
-
-  // Calculate x domain (range of years)
-  var yearsArr = d3.keys(yearsSet).sort();
-  var years = d3.range(+yearsArr[0], +yearsArr[yearsArr.length - 1] + 1);
-
-  // Transform histograms to series
-  var series = d3.keys(counts).map(function(cluster) {
-    return years.map(function(year) { return { x: year, y: counts[cluster][year] ? counts[cluster][year] : 0 }; }); 
-  });
-
-  return { series: series, labels: d3.keys(clusters) };
-}
-
-/**
- Returns company -> (year -> freq)
- i.e. companies are bose, hiwave etc...
-*/
-function getSeries_company() {
-
-  function randomPriDate(priDateStr) {
-    // apply noise to priDate (ensure result within [1965, 2012]
-    var priYr = +priDateStr.slice(0, 4);
-    var delta_max = 2013 - priYr;
-    var delta_min = Math.min(0, 1965 - priYr);
-    var delta = Math.floor(delta_min + (delta_max - delta_min) * Math.random());
-    var nYear = priYr + delta;
-    return nYear + "" + priDateStr.slice(4);    
-  }
-  
-  function getFakeData(patFams) {
-    return patFams.map(function(p) {
-      var np = $.extend(true, {}, p);
-      np.priorityDate = randomPriDate(np.priorityDate);
-      return np;
-    });
-  }
-  
-  // Get pFams per company
-  var bose_pFams   = d3.values(eqip.bosePatFams);
-  var hiwave_pFams = d3.values(eqip.hiwavePatfams);
-  var companies = {
-    "Bose"            : bose_pFams,
-    "Hiwave"          : hiwave_pFams,
-    "Global Omnicorp" : getFakeData(bose_pFams),
-    "Patently Obvious PLC" : getFakeData(bose_pFams),
-    "Kinda Big Deal inc." : getFakeData(bose_pFams),
-    "Holding Holdings" : getFakeData(bose_pFams),
-    "Acme LLC"        : getFakeData(bose_pFams)
-  };
-
-  // Construct histograms: company -> (year -> freq)
-  var counts = d3.keys(companies).reduce(function(acc, d) { acc[d] = {}; return acc; }, {}),
-      yearsSet = {},
-      inc = function(o, k) { o[k] = o[k] ? o[k] + 1 : 1; };
-  d3.keys(companies).forEach(function(c) {
-    companies[c].forEach(function(p) {
-      var priYr = p.priorityDate.slice(0, 4);
-      yearsSet[priYr] = true;
-      inc(counts[c], priYr);
-    });
-  });
-
-  // Calculate x domain (range of years)
-  var yearsArr = d3.keys(yearsSet).sort();
-  var years = d3.range(+yearsArr[0], +yearsArr[yearsArr.length - 1] + 1);
-
-  // Transform histograms to series
-  var series = d3.keys(counts).map(function(company) {
-    return years.map(function(year) { return { x: year, y: counts[company][year] ? counts[company][year] : 0}; });
-  });
-
-  return { series: series, labels: d3.keys(companies) };  
-}
-
-
 /*
  Simulate variation in Bose Cluster 0,
  by randomly removing some grant dates.
@@ -515,6 +410,12 @@ var regs_boseClstr1_byOutcome = series.registrationsPerYearPerOutcome(bosePfamsP
 var regs_bose                 = series.registrationsPerYear(boseAllPfams); // single series
 var regs_bose_byCluster       = series.registrationsPerYearPerKey(bosePfamsPerNamedCluster);
 var regs_bose_byCompany       = series.registrationsPerYearPerKey(pFamsPerCompany);
+
+// Get models: CUMULATIVE REGISTRATIONS (only accepted)
+var cuml_boseClstr1_byOutcome = series.cumlRightsPerYearPerOutcome(bosePfamsPerCluster[0] /* cluster 1 */);
+// var cuml_bose                 = series.cumlRightsPerYear(boseAllPfams); // single series
+// var cuml_bose_byCluster       = series.cumlRightsPerYearPerKey(bosePfamsPerNamedCluster);
+// var cuml_bose_byCompany       = series.cumlRightsPerYearPerKey(pFamsPerCompany);
 
 
 // Render charts
